d1b8f81a16cd3df3fd1aa1d33639530a1c3d2875
tom-dq <32104749+tom-dq@users.noreply.github.com>
2020-11-18 16:31:08 +1100

    WIP

--- stent_opt/struct_opt/optimisation_parameters.py
+++ stent_opt/struct_opt/optimisation_parameters.py
@@ -153,7 +153,7 @@ def _clamp_update(old, new, max_delta):
 
 
 active = OptimParams(
-    max_change_in_vol_ratio=0.0025,
+    max_change_in_vol_ratio=0.0015,  # Was 0.0025
     volume_target_opts=VolumeTargetOpts(
         initial_ratio=0.12,
         floor_ratio=0.05,
@@ -179,7 +179,7 @@ active = OptimParams(
     abaqus_output_time_interval=0.1,
     abaqus_target_increment=1e-6,
     release_stent_after_expansion=False,
-    analysis_step_type=step.StepDynamicImplicit,
+    analysis_step_type=step.StepDynamicExplicit,
 )
 
 
