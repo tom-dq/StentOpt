45d4657d824d6bb8b54a51aa6f48437109aee9e9
Tom Wilson <tom@getdqd.com>
2021-02-25 11:37:28 +1100

    WIP

--- stent_opt/struct_opt/computer.py
+++ stent_opt/struct_opt/computer.py
@@ -42,7 +42,7 @@ elif psutil.cpu_count(logical=False) == 8:
 elif psutil.cpu_count(logical=False) == 6:
     # Macbook - just for viewing results at the moment.
     n_cpus = 4
-    base_working_dir = r"/Users/tomwilson/PhD Data/StentOptSims"
+    base_working_dir = r"/Users/tomwilson/Dropbox/PhD/StentOptDBs"
     fig_size = (1200, 750)  # (2000, 1350)
 
 else:
