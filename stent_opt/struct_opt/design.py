0efdce368d4f87fac270221245dfa0dfded5a6dd
Tom Wilson <tom.wilson66@gmail.com>
2022-07-08 10:41:06 +1000

    designs

--- stent_opt/struct_opt/design.py
+++ stent_opt/struct_opt/design.py
@@ -1601,9 +1601,9 @@ bc_shear = (_bc_fix_left, _bc_shear_right)
 
 bc_fix_left_edge = BoundaryCond(th_min=0.0, th_max=0.0, z_min=0.0, z_max=1.0, bc_th=True, bc_z=False, load_factor_scale=0)
 bc_strain_right_edge = BoundaryCond(th_min=1.0, th_max=1.0, z_min=0.35, z_max=0.65, bc_th=True, bc_z=False, load_factor_scale=1)
-bc_fix_bottom_left = BoundaryCond(th_min=0.0, th_max=0.0, z_min=0.0, z_max=0.0, bc_th=True, bc_z=True, load_factor_scale=0)
+bc_fix_bottom_left = BoundaryCond(th_min=0.0, th_max=0.1, z_min=0.0, z_max=0.1, bc_th=True, bc_z=True, load_factor_scale=0)
 
-bcs_pull_30pc = (bc_fix_left_edge, bc_strain_right_edge, bc_fix_bottom_left)
+bcs_pull_30pc = (_bc_fix_left, bc_strain_right_edge)
 
 
 bc_cent_load_enf_disp = BoundaryCond(th_min=0.5, th_max=0.5, z_min=0.0, z_max=1.0, bc_th=False, bc_z=True, load_factor_scale=-1)
@@ -1615,8 +1615,8 @@ _spring_corner = _SpringRegion(min_val_k=1.0, max_val_k=0.0, target_region_k=100
 bc_simon_spring = (bc_cent_load_enf_disp, bc_cent_bottom_20pc_A.copy_with_updates(spring_at_min_max_and_overall=_spring_corner), bc_cent_bottom_20pc_B.copy_with_updates(spring_at_min_max_and_overall=_spring_corner))
 
 bc_pull_bottom = BoundaryCond(th_min=0.9, th_max=1.0, z_min=0.0, z_max=0.0, bc_th=False, bc_z=True, load_factor_scale=-1)
-bc_canti = (bc_cent_bottom_20pc_A, bc_cent_bottom_20pc_B, bc_pull_bottom)
-NOMINAL_ELEMENTS = 70
+bc_canti = (bc_fix_left_edge, bc_cent_bottom_20pc_A, bc_cent_bottom_20pc_B, bc_pull_bottom)
+NOMINAL_ELEMENTS = 32
 
 
 centre_sym_enf_disp = StentParams(
@@ -1655,7 +1655,7 @@ two_by_one_cantilever_test = StentParams(
     stent_element_type=element.ElemType.CPS4R,
     balloon=None,
     cylinder=None,
-    expansion_ratio=1.05,  # 2.0
+    expansion_ratio=1.25,  # 2.0
     inadmissible_regions=tuple(),
     boundary_conds=bc_canti,
     end_connection_length_ratio=0.3,
@@ -1678,7 +1678,7 @@ simple_shear_test = StentParams(
     stent_element_type=element.ElemType.CPS4R,
     balloon=None,
     cylinder=None,
-    expansion_ratio=1.05,  # 2.0
+    expansion_ratio=2.0,  # 2.0
     inadmissible_regions=tuple(),
     boundary_conds=bc_shear,
     end_connection_length_ratio=0.3,
@@ -1688,7 +1688,30 @@ simple_shear_test = StentParams(
     fix_base=False,
 )
 
-basic_stent_params = simple_shear_test
+simple_pull = StentParams(
+    angle=60,
+    divs=PolarIndex(
+        R=1,
+        Th=2*NOMINAL_ELEMENTS,
+        Z=NOMINAL_ELEMENTS,
+    ),
+    r_min=0.65,
+    r_max=0.75,
+    length=1.65 / 4,  # Was 11.0
+    stent_element_type=element.ElemType.CPS4R,
+    balloon=None,
+    cylinder=None,
+    expansion_ratio=2.0,  # 2.0
+    inadmissible_regions=tuple(),
+    boundary_conds=bcs_pull_30pc,
+    end_connection_length_ratio=0.3,
+    whole_left_side_restrained=False,
+    sym_x=False,
+    sym_y=False,
+    fix_base=False,
+)
+
+basic_stent_params = two_by_one_cantilever_test
 
 
 if __name__ == "__main__":
