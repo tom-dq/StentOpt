91195ccbe567fd4c542b16230e33f0d93391c2ca
Tom Wilson <tom@getdqd.com>
2022-10-13 12:24:33 +1100

    Format with black

--- stent_opt/struct_opt/display.py
+++ stent_opt/struct_opt/display.py
@@ -15,14 +15,15 @@ import stent_opt.struct_opt.score
 from stent_opt.struct_opt import design, generation
 from stent_opt.abaqus_model import base
 
+
 class ViewAngle(typing.NamedTuple):
     elev: float
     azim: float
 
 
-_VIEW_SIDE = ViewAngle(elev=17., azim=-20.)
-_VIEW_TOP = ViewAngle(elev=60., azim=0.)
-_VIEW_2D = ViewAngle(elev=90., azim=0.)
+_VIEW_SIDE = ViewAngle(elev=17.0, azim=-20.0)
+_VIEW_TOP = ViewAngle(elev=60.0, azim=0.0)
+_VIEW_2D = ViewAngle(elev=90.0, azim=0.0)
 VIEW_ANGLE = _VIEW_2D
 
 
@@ -67,14 +68,19 @@ class BackgroundColour:
         c = colorsys.rgb_to_hls(*mcolors.to_rgb(c))
         return colorsys.hls_to_rgb(c[0], max(0, min(1, AMOUNT * c[1])), c[2])
 
+
 BACKGROUND_COLOUR_TRACKER = BackgroundColour()
 
 
 def render_status(
-        stent_design: "design.StentDesign",
-        node_position: typing.Dict[int, base.XYZ],
-        components: typing.Iterable[typing.Union["generation.PrimaryRankingComponent", "generation.SecondaryRankingComponent"]],
-        title_prefix: str,
+    stent_design: "design.StentDesign",
+    node_position: typing.Dict[int, base.XYZ],
+    components: typing.Iterable[
+        typing.Union[
+            "generation.PrimaryRankingComponent", "generation.SecondaryRankingComponent"
+        ]
+    ],
+    title_prefix: str,
 ):
 
     components = list(components)
@@ -88,12 +94,23 @@ def render_status(
 
     # Get all the faces of the stent elements
     def faces_and_values():
-        elem_num_to_indices = {iElem: idx for iElem, idx in design.generate_elem_indices(stent_design.stent_params.divs)}
-        elem_lookup = {elem_num_to_indices[comp.elem_id]: comp.value for comp in components}
+        elem_num_to_indices = {
+            iElem: idx
+            for iElem, idx in design.generate_elem_indices(
+                stent_design.stent_params.divs
+            )
+        }
+        elem_lookup = {
+            elem_num_to_indices[comp.elem_id]: comp.value for comp in components
+        }
 
         for one_elem in stent_design.active_elements:
-            elem_connection = design.get_c3d8_connection(stent_design.stent_params.divs, one_elem)
-            for one_face_idx in stent_opt.struct_opt.score.FACES_OF[stent_design.stent_params.stent_element_type]:
+            elem_connection = design.get_c3d8_connection(
+                stent_design.stent_params.divs, one_elem
+            )
+            for one_face_idx in stent_opt.struct_opt.score.FACES_OF[
+                stent_design.stent_params.stent_element_type
+            ]:
                 global_node_nums = [elem_connection[i] for i in one_face_idx]
                 yield global_node_nums, elem_lookup[one_elem]
 
@@ -101,17 +118,18 @@ def render_status(
     faces_paired = collections.defaultdict(list)
     for global_node_nums, value in faces_and_values():
         sorted_node_nums = tuple(sorted(global_node_nums))
-        faces_paired[sorted_node_nums].append( (global_node_nums, value) )
+        faces_paired[sorted_node_nums].append((global_node_nums, value))
 
-    single_faces_and_vals = [one_list[0] for one_list in faces_paired.values() if len(one_list) == 1]
-
+    single_faces_and_vals = [
+        one_list[0] for one_list in faces_paired.values() if len(one_list) == 1
+    ]
 
     # Render it
     poly_list = []
     for face_nodes, val in single_faces_and_vals:
         verts = [node_position[iNode] for iNode in face_nodes]
 
-        poly_list.append( (verts, val))
+        poly_list.append((verts, val))
 
     bg_col = BACKGROUND_COLOUR_TRACKER.get_colour(comp_type)
 
@@ -123,7 +141,11 @@ def show_design(stent_design: "design.StentDesign"):
 
     # Oh gawd but this is a one off I swear
     from stent_opt.struct_opt.design import generate_nodes, generate_stent_part_elements
-    node_position = {iNode: xyz.to_xyz() for iNode, polar_index, xyz in generate_nodes(stent_design.stent_params)}
+
+    node_position = {
+        iNode: xyz.to_xyz()
+        for iNode, polar_index, xyz in generate_nodes(stent_design.stent_params)
+    }
 
     poly_list = []
     for idx, iElem, elem in generate_stent_part_elements(stent_design.stent_params):
@@ -132,9 +154,6 @@ def show_design(stent_design: "design.StentDesign"):
             poly_list.append((verts, 1.0))
 
     show_polygons(f"show_design", poly_list, (0.9, 0.9, 0.9), dims=2)
-
-
-
 
 
 def show_polygons(title, poly_list, bg_col, dims: int):
@@ -183,16 +202,15 @@ def show_polygons(title, poly_list, bg_col, dims: int):
     else:
         all_verts_2d = []
         for one_poly in all_verts:
-            verts_2d =  [(x, y) for x, y, z in one_poly]
+            verts_2d = [(x, y) for x, y, z in one_poly]
             all_verts_2d.append(verts_2d)
         tri = PolyCollection(all_verts_2d)
 
     tri.set_facecolors(all_colours)
     # tri.set_edgecolor('k')
 
-
     ax.set_facecolor(bg_col)
-    #fig.patch.set_facecolor(bg_col)
+    # fig.patch.set_facecolor(bg_col)
 
     if is_3d:
         ax.add_collection3d(tri)
@@ -200,15 +218,16 @@ def show_polygons(title, poly_list, bg_col, dims: int):
         ax.add_collection(tri)
 
     # Colourbar
-    axins = inset_axes(ax,
-                        width="75%",  # width = 50% of parent_bbox width
-                        height="2%",  # height : 5%
-                        loc='lower center',
-                        bbox_to_anchor=(0, 0.05, 1, 1),
-                       bbox_transform=ax.transAxes,
-                        )
+    axins = inset_axes(
+        ax,
+        width="75%",  # width = 50% of parent_bbox width
+        height="2%",  # height : 5%
+        loc="lower center",
+        bbox_to_anchor=(0, 0.05, 1, 1),
+        bbox_transform=ax.transAxes,
+    )
 
-    fig.colorbar(colour_mapping, cax=axins, orientation='horizontal')
+    fig.colorbar(colour_mapping, cax=axins, orientation="horizontal")
 
     if is_3d:
         ax.set_xlim3d(*get_lim(0))
@@ -219,8 +238,6 @@ def show_polygons(title, poly_list, bg_col, dims: int):
         ax.set_xlim(*get_lim(0))
         ax.set_ylim(*get_lim(1))
 
-
-
     if is_3d:
         # Hide the 3D axis
         ax.set_axis_off()
@@ -228,9 +245,6 @@ def show_polygons(title, poly_list, bg_col, dims: int):
         ax.elev = VIEW_ANGLE.elev
 
     pl.show()
-
-
-
 
 
 if __name__ == "__main__":
